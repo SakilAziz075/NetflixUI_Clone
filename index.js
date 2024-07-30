52f6e61ada944eab37b37e90aa29d5a0eb6805a1
Sakil Aziz <sakilaziz12@gmail.com>
2024-07-30 23:22:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById("emailButton-1").addEventListener("click" , function(){
+    var inputToHighlight = document.getElementById("emailInput-1");
+    inputToHighlight.classList.add("glow");
+} );
+
